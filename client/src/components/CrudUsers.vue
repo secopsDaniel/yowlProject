6aed6047024774ff9c2c93f54776ba8ef3b9af53
Elsa rabé <elsa.rabe@epitech.eu>
2026-04-18 18:52:59 +0000

    Crud users

---
+++ client/src/components/CrudUsers.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- component -->
+<div class="text-gray-900 bg-blue-200">
+    <div class="p-4 flex">
+        <h1 class="text-3xl">Utilisateurs</h1>
+    </div>
+    <div class="px-3 py-4 flex justify-center">
+        <table class="w-full text-md bg-white shadow-md rounded mb-4">
+            <tbody>
+                <tr class="border-b">
+                    <th class="text-left p-3 px-5">Name</th>
+                    <th class="text-left p-3 px-5">Email</th>
+                    <th class="text-left p-3 px-5">birthday</th>
+                    <th class="text-left p-3 px-5">Role</th>
+                    <th></th>
+                </tr>
+                <tr class="border-b hover:bg-blue-100 bg-gray-100">
+                    <td class="p-3 px-5">
+                        <input type="text" value="user.name" class="bg-transparent border-b-2 border-gray-300 py-2">
+                    </td>
+                    <td class="p-3 px-5">
+                        <input type="text" value="user.email" class="bg-transparent border-b-2 border-gray-300 py-2">
+                    </td>
+                    <td class="p-3 px-5">
+                        <input type="text" value="birthday" class="bg-transparent border-b-2 border-gray-300 py-2">
+                    </td>
+                    <td class="p-3 px-5">
+                        <select value="user.role" class="bg-transparent border-b-2 border-gray-300 py-2">
+                            <option value="user">user</option>
+                            <option value="admin">admin</option>
+                        </select>
+                    </td>
+                    <td class="p-3 px-5 flex justify-end">
+                        <button type="button"
+                            class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button><button
+                            type="button"
+                            class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</div>
+</template>
---
+++ client/src/views/Dashbord.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Admin from '@/components/admin.vue';
+import CrudUsers from '@/components/CrudUsers.vue';
+</script>
+
+<template>
+  <div>
+     <Admin/>
+  </div>
+</template>
